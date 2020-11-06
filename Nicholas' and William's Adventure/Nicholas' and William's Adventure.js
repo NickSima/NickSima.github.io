da5d142856ce4b4d5f1e4ddb20c46cdf775c8952
NickSima <74073860+NickSima@users.noreply.github.com>
2020-11-06 12:24:09 -0800

    First Upload

---
+++ Nicholas' and William's Adventure/Nicholas' and William's Adventure.js
@@ -0,0 +1,103 @@
+const levels = [
+				//level 0
+				["flag", "rock", "", "", "",
+				"fenceside", "rock", "", "", "rider",
+				"", "tree", "animate", "animate", "animate",
+				"", "water", "", "", "",
+				"", "fence", "", "horseup", "",],
+
+				//level 1
+				["flag", "water", "", "", "",
+				"fenceside", "water", "", "", "rider",
+				"animate", "bridge animate", "animate", "animate", "animate",
+				"", "water", "", "", "",
+				"", "water", "horseup", "", "",],
+
+				//level 2
+				["tree", "tree", "flag", "tree", "tree",
+				"animate", "animate", "animate", "animate", "animate",
+				"water", "bridge", "water", "water", "water",
+				"", "", "", "fence", "",
+				"rider", "rock", "", "", "horseup",]
+				]; //end of levels
+
+var gridBoxes;
+var currentLevel = 0; //starting level
+var riderOn = false; //is the rider on?
+var currentLocationOfHorse = 0;
+var currentAnimation; //allows 1 animation per level
+
+//start game
+window.addEventListener("load", function () {
+	gridBoxes = document.querySelectorAll("#gameBoard div");
+	loadLevel();
+});
+
+//load levels 0 - max level
+function loadLevel(){
+	let levelMap = levels[currentLevel];
+	let animateBoxes;
+	riderOn = false;
+
+	//load board
+	for (i = 0; i < gridBoxes.length; i++) {
+		gridBoxes[i].className = levelMap[i];
+		if (levelMap[i].includes("horse")) currentLocationOfHorse = i;
+	}//for
+
+	animateBoxes = document.querySelectorAll(".animate");
+
+	animateEnemy(animateBoxes, 0, "right");
+
+}//loadLevel
+
+//animate enemy left and right
+//boxes
+//index
+//direction
+function animateEnemy(boxes, index, direction) {
+
+	//exit function if no animation
+	if (boxes.length <= 0) {return;}
+
+	//update images
+	if(direction == "right") {
+	boxes[index].classList.add("enemyright");
+	} else if (direction == "left") {
+	boxes[index].classList.add("enemyleft");
+	}//if else
+
+	//remove images from other boxes
+	for (i = 0; i < boxes.length; i++) {
+		if (i != index) {
+			boxes[index].classList.remove("enemyright");
+			boxes[index].classList.remove("enemyleft");
+		}//if
+	}//for
+
+	//moving
+	if (direction == "left") {
+		//turn around if hit side
+		if (index == boxes.length - 1){
+			index--;
+			direction = "right";
+		} else {
+			index++;
+		}
+	} else if (direction == "right") {
+		//turn around if hit side
+		if (index == 0){
+			index++;
+			direction = "right";
+		} else {
+			index--;
+		}// else if
+
+	}//else if
+
+	currentAnimation = setTimeout(function() {
+	animateEnemy(boxes, index, direction);
+	}, 750);
+
+
+}//animate Enemy
